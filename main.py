5bf781fa0ede5f000e3c8c8944bdce84d927f1c7
Shira Stromer <shirabroner@Shiras-MacBook-Pro.local>
2026-02-05 12:43:05 -0500

    bio

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+
+llm = ChatOpenAI(temperature=0.9)
+
 def main():
-    print("Hello, World!")
+    # read txt from elon_musk.txt
+    elon_musk_bio = ""
+    with open("elon_musk_info.txt", "r") as f:
+        elon_musk_bio = f.read()
+
+    summary_template = """
+    Given the information {bio}, provide:
+        1. a concise summary of the key points about Elon Musk.
+        2. 2 interesting facts about Elon Musk.
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["bio"],
+        template=summary_template,
+    )
+
+    llm = ChatOpenAI(temperature=0)
+    print(llm.invoke(
+        prompt.format(bio=elon_musk_bio)
+    ).content)
 
 if __name__ == "__main__":
     main()
