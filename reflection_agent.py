c43f4d9a14d9660e24e0be88b2e098fc6ca16553
Shira Stromer <shirabroner@Shiras-MacBook-Pro.local>
2026-02-26 13:21:09 -0500

    reflection agent

---
+++ reflection_agent.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, StateGraph, add_messages
+
+load_dotenv()
+
+REFLECT, GENERATE = "reflect", "generate"
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
+            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            " Generate the best twitter post possible for the user's request."
+            " If the user provides critique, respond with a revised version of your previous attempts.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+
+llm = ChatOpenAI()
+generate_chain = generation_prompt | llm
+reflect_chain = reflection_prompt | llm
+
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def generation_node(state: MessageGraph) -> str:
+    return {"messages": [generate_chain.invoke({"messages": state["messages"]})]}
+
+def reflection_node(state: MessageGraph) -> str:
+    res = reflect_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
+
+def should_continue_generate(state: MessageGraph) -> str:
+    if len(state["messages"]) > 6:
+        return END    
+    return REFLECT
+
+builder = StateGraph(state_schema=MessageGraph)
+
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+builder.add_conditional_edges(GENERATE, should_continue_generate, path_map={REFLECT: REFLECT, END: END})
+builder.add_edge(REFLECT, GENERATE)
+
+app = builder.compile()
+print(app.get_graph().draw_mermaid())
+
+if __name__ == "__main__":
+    pass
